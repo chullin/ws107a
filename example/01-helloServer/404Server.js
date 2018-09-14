c7b744a0d55d7436bd64215a214ba986a9e7b680
chullin <fg6ts15@gmail.com>
2018-09-14 15:58:10 +0800

    Add files via upload

--- example/01-helloServer/404Server.js
+++ example/01-helloServer/404Server.js
@@ -3,9 +3,8 @@ const http = require('http');  // 引用 http 這個模組，node 專用
 const port = 3000, hostname = 'localhost' // 創建
                               // 請求, 回應 (request, response)
 const server = http.createServer((req, res) => {  //回呼函數
-  res.statusCode = 200;  // http 回傳 200 代表網頁正常
-  res.setHeader('Content-Type', 'text/plain'); // text/plain回傳網頁為純文字
-  res.end('Hello World\n'); // end 可以回傳一個訊息
+  res.statusCode = 404;  
+  res.end();
 });
 
 server.listen(port, () => {
--- example/01-helloServer/hellohtmlServer2.js
+++ example/01-helloServer/hellohtmlServer2.js
@@ -1,13 +1,15 @@
 const http = require('http');  // 引用 http 這個模組，node 專用
 
 const port = 3000, hostname = 'localhost' // 創建
-                              // 請求, 回應 (request, response)
+
 const server = http.createServer((req, res) => {  //回呼函數
   res.statusCode = 200;  // http 回傳 200 代表網頁正常
-  res.setHeader('Content-Type', 'text/plain'); // text/plain回傳網頁為純文字
-  res.end('Hello World\n'); // end 可以回傳一個訊息
+  res.setHeader('Content-Type', 'text/html'); // text/plain回傳網頁為純文字
+  res.end('<html><head></head><body><p>Hello World\n</p> <a href="http://tw.youtube.com">Youtube</a>\n</body><//html>'); // end 可以回傳一個訊息
 });
 
 server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
